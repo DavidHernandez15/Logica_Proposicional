409a8500be35eb2c84fe0552d1277f46214745c7
DavidHernandez15 <98990320+DavidHernandez15@users.noreply.github.com>
2023-09-14 17:50:19 -0500

    Primer commit
    Versión inicial

---
+++ Logica_computacional/LogicaProposicional.dpr
@@ -0,0 +1,13 @@
+program LogicaProposicional;
+
+uses
+  Forms,
+  uPrincipal in 'uPrincipal.pas' {Form1};
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
